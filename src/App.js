b733d842edad197853f5fdff1a34b7dc2b6e39ce
Aanttrax <Arielrt20@gmail.com>
2022-06-25 11:08:55 -0400

    prueba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [daysExpenditures, setDaysExpenditures]=useState(1);
+  const [daysTrailing, setDaysTrailing]=useState(1);
+  const [notify,setNotify]=useState();
+  const [error, setError]=useState(false);
+
+  function expenditures(e){
+    if(e.target.value < 1){
+      setError(true)
+    }
+    setDaysExpenditures(e.target.value);
+    setError(false)
+  }
+  function days(e){
+    if(e.target.value < 1){
+      setError(true)
+    }
+    setDaysTrailing(e.target.value);
+    setError(false)
+  }
+
+  let numbers = [];
+
+  for (let i = 0; i<daysExpenditures; i++){
+    numbers.push(<input id = {`days_${i}`} type='number' key={i}/>)
+  }
+
+  function play(e){
+    e.preventDefault();
+    let expenditures_num = [];
+    let notice =0;
+    
+    for(let i =0;i<numbers.length;i++){
+      expenditures_num.push(parseInt(document.getElementById(`days_${i}`).value,10))
+    }
+
+    let aux = 0;
+    for(let i=0;i<expenditures_num.length-daysTrailing;i++){
+      i=parseInt(daysTrailing)+i;
+      let average = 0;  
+      let k =0;
+      console.log(aux,'aux')
+      for(let j=0;j<daysTrailing;j++){
+        
+        k=k+aux
+        average = average + expenditures_num[k];
+        k++;
+        k=k-aux;
+        console.log(k,'k')
+      }
+      console.log(average/daysTrailing,'prom')
+      if(expenditures_num[i]>= (2*average/daysTrailing)){
+        notice++
+        console.log('mayorrrr',notice)
+      }
+      i=i-parseInt(daysTrailing);
+      aux++;
+      setNotify(notice)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <label htmlFor="expenditures">Number of Days of Transaction:</label>
+      <input onChange={expenditures} type='number' id="expenditures" min="1" max="200000"/>
+      <label htmlFor="days">Number of Trailing Days:</label>
+      <input onChange={days} type='number' id='days' min='1'/>
+      <form className='form' onSubmit={play}>
+        <div>
+          {numbers}
+        </div>
+        {error?<p>Wrong number</p>:''}
+        <button className="btn">Play</button>
+      </form>
+      <div>
+      {notify?<p>number of notification {notify}</p>:''}
+      </div>
     </div>
   );
 }
